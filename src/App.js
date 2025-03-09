98b80b5c60658ec82eecb44a7451795330201925
Shubham Arora <shubhamarora.devs@gmail.com>
2025-03-09 16:14:39 +0530

    final implementation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
-  let inputLength = 5;
+  let inputLength = 4;
 
   const [otpInputs, setOtpInputs] = useState(new Array(inputLength).fill(""));
-  console.log("otpInputs", otpInputs);
-  const inputRef = useRef(null);
+  const inputRef = useRef([]);
+
+  useEffect(() => {
+    if (inputRef.current[0]) {
+      inputRef.current[0].focus();
+    }
+  }, []);
   const handleChange = (id, value) => {
-    console.log("id, value", id, value);
-    inputRef.current = (id + 1)%inputLength;
-    
+    if (isNaN(value)) return;
+
+    let newOtp = [...otpInputs];
+    newOtp[id] = value.substring(value.length - 1);
+
+    setOtpInputs(newOtp);
+
+    if (id < inputLength - 1 && value && inputRef.current[id + 1]) {
+      inputRef.current[id + 1].focus();
+    }
+  };
+  const onClick = (index, event) => {
+    inputRef.current[index].setSelectionRange(1,1);
+  };
+
+  const onKeyDown = (index, event) => {
+    if(event.key == "Backspace"){
+        if(index > 0){
+            const newOtp = [...otpInputs];
+            newOtp[index] = "";
+            setOtpInputs(newOtp);
+            inputRef.current[index-1].focus();
+        }
+    }
   };
 
   return (
     <>
       <div>Otp functionality</div>
-      {otpInputs.map((_, idx) => (
+      {otpInputs.map((value, idx) => (
         <input
           id={idx}
-          ref={inputRef}
-          className={`inputField ${"currentId" === idx ? "_active" : ""}`}
-          onChange={(e) => handleChange(_, e.target.value)}
+          key={idx}
+          ref={(input) => (inputRef.current[idx] = input)}
+          value={value}
+          className={`otpInput`}
+          onChange={(e) => handleChange(idx, e.target.value)}
+          onClick={(e) => onClick(idx, e)}
+            onKeyDown={(e) => onKeyDown(idx, e)}
         />
       ))}
     </>
